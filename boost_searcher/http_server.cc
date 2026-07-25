43ebf5825f42da841b781d9a5d460e69773098e6
Flipped522 <example@163.com>
2026-07-25 08:56:39 +0800

    httpserver

--- boost_searcher/http_server.cc
+++ boost_searcher/http_server.cc
@@ -1,7 +1,16 @@
 #include "searcher.hpp"
+#include "cpphttplib/httplib.h"
+
+const std::string root_path = "./wwwroot";
 
 int main()
 {
+    httplib::Server svr;
 
+    svr.set_base_dir(root_path.c_str());
+    svr.Get("/hi", [](const httplib::Request &req, httplib::Response &rsp){
+        rsp.set_content("必须要有搜索关键字!", "text/plain; charset=utf-8");
+    });
+    svr.listen("0.0.0.0", 8081);
     return 0;
 }
