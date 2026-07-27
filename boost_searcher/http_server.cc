13631fffed7946e3763f9b352d6220d32932911a
Flipped522 <example@163.com>
2026-07-27 08:22:55 +0800

    httpserver

--- boost_searcher/http_server.cc
+++ boost_searcher/http_server.cc
@@ -1,16 +1,28 @@
 #include "searcher.hpp"
 #include "cpphttplib/httplib.h"
 
+const std::string input = "data/raw_html/raw.txt";
 const std::string root_path = "./wwwroot";
 
 int main()
 {
-    httplib::Server svr;
+    ns_searcher::Searcher search;
+    search.InitSearcher(input);
 
+    httplib::Server svr;
     svr.set_base_dir(root_path.c_str());
-    svr.Get("/hi", [](const httplib::Request &req, httplib::Response &rsp){
-        rsp.set_content("必须要有搜索关键字!", "text/plain; charset=utf-8");
-    });
+    svr.Get("/s", [&search](const httplib::Request &req, httplib::Response &rsp)
+            {
+        if(!req.has_param("word"))
+        {
+            rsp.set_content("必须要有搜索关键字!", "text/plain; charset=utf-8");
+            return;
+        }
+        std::string word = req.get_param_value("word");
+        std::cout << "用户在搜索： " << word << std::endl;
+        std::string json_string;
+        search.Search(word, &json_string);
+        rsp.set_content(json_string.c_str(), "application/json"); });
     svr.listen("0.0.0.0", 8081);
     return 0;
 }
